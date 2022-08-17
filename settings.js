473bbd584170a4e68266c7b2fa83dadcaf99b41e
shikimorii <109281379+shikimorii@users.noreply.github.com>
2022-08-17 20:32:38 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -31,7 +31,7 @@ global.APIs = {
 
 //Api Key\\
 global.APIKeys = {
-	'https://zenzapis.xyz': 'your key',
+	'https://zenzapis.xyz': '766666e4e96b',
 }
 
 
@@ -54,23 +54,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.vcardowner = ['6281391486982'] //ur owner number
+global.ownername = "introvert" //ur owner name
+global.ytname = "YT: " //ur yt chanel name
+global.socialm = "GitHub: shikimorii" //ur github or insta name
+global.location = "Indonesia" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
-global.reactmoji = "😘" //ur menu react emoji
-global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.owner = ['6281391486982']
+global.ownertag = '6281391486982' //ur tag number
+global.botname = 'Introvert-bot' //ur bot name
+global.linkz = "https://youtu.be" //your theme url which will be displayed on whatsapp
+global.websitex = "https://youtu.be" //ur website to be displayed
+global.botscript = 'https://github.com' //script link
+global.reactmoji = "🤔" //ur menu react emoji
+global.themeemoji = "🤔" //ur theme emoji
+global.packname = "introvert🤔" //ur sticker watermark packname
+global.author = "introvert🤔" //ur sticker watermark authoin
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
@@ -79,7 +79,7 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['916909137213'] //ur premium numbers
+global.premium = ['6281391486982'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
@@ -101,7 +101,7 @@ global.ntilink = []
 
 //omther
 global.sessionName = 'session'
-global.antitags = true
+global.antitags = false
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
